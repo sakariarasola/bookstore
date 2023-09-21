67b76abd42031b809d30f2821ad7efb9414632ab
Sakari Arasola <sagary_97@hotmail.com>
2023-09-21 20:36:17 +0300

    REST tehtävät lisätty

--- src/main/java/com/example/web/BookController.java
+++ src/main/java/com/example/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.domain.Book;
 import com.example.domain.BookRepository;
@@ -29,6 +33,16 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@GetMapping(value = { "books" })
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+
+	@GetMapping(value = "book/{id}")
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
 	}
 
 	@GetMapping(value = { "addbook" })
